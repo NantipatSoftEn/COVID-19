4ef826934df1b7e031b65c426c9c027c40a4ae12
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-26 20:07:36 +0700

    Check point

---
+++ src/components/Layout/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Container = () => {
+    return (
+        <div>Hello bos</div>
+    )
+}
+
+export default  Container
---
+++ src/components/Layout/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Container from './Container';
+const Main = () => {
+  return (
+    <div className="bg-gray-100 font-sans leading-normal tracking-normal">
+      <Navbar />
+      <Container />
+    </div>
+  );
+};
+export default Main;
