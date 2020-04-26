93cd0e7800c81c8c126ffb1fc599882a169b7ca3
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-26 20:57:02 +0700

    Layout/Navbar:  Add  Hambergerr

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -11,6 +11,19 @@ const Navbar = () => {
             <i class="fas fa-sun text-orange-600 pr-3"></i> Admin Day Mode
           </a>
         </div>
+        <div class="block lg:hidden pr-4">
+          <button
+            id="nav-toggle"
+            class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-teal-500 appearance-none focus:outline-none">
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
         <SubMenu />
       </div>
     </nav>
