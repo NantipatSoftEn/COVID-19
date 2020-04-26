76d40c1ee8bfb515d0c3f6c317a46593dd54344b
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-26 10:57:25 +0700

    tailwind it work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
   return (
     <div>
-      Hello boyss
+      <h1 class="text-4xl font-bold text-center text-blue-500">
+        Hello Tailwind!
+      </h1>
     </div>
   );
 }
