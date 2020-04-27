bfdb8ad4f093f8768bb3aa6d7cf5237ca71cd545
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-28 00:23:08 +0700

    Layout:  Set Them

---
+++ src/Layout/CardChart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const CardChart = () => {
+  return (
+    <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+      <div class="bg-white border rounded shadow">
+        <div class="border-b p-3">
+          <h5 class="font-bold uppercase text-gray-600">Template</h5>
+        </div>
+        <div class="p-5"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CardChart;
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Container from './Container';
+import CardChart from './CardChart';
+import Footer from './Footer'
+import Table from './Table'
 const Main = () => {
   return (
     <div className="bg-gray-100 font-sans leading-normal tracking-normal">
       <Navbar />
       <Container />
+      <hr className="border-b-2 border-gray-400 my-8 mx-4" />
+      <CardChart />
+      <Table />
+      <Footer />
     </div>
   );
 };
